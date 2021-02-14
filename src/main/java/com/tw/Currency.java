3b7e60501d1b2527e2edb5bd84dd51b83450ff9a
nishthapaul <nishtha.paul@thoughtworks.com>
2021-02-14 22:43:25 +0530

    Refractor | Clubbed currency conversion methods to single method

--- src/main/java/com/tw/Currency.java
+++ src/main/java/com/tw/Currency.java
@@ -1,7 +1,7 @@
 package com.tw;
 
 enum Currency {
-    RUPEE(1), DOLLAR(74.85);
+    RUPEE(1), DOLLAR(74.85), YEN(0.7);
 
     private final double conversionFactor;
 
@@ -10,18 +10,6 @@ enum Currency {
     }
 
     public double convertTo(Currency currency, double amount) {
-        if(currency == RUPEE) {
-            return convertToBaseCurrency(amount);
-        } else {
-            return convertToNonBaseCurrency(currency, amount);
-        }
-    }
-
-    private double convertToBaseCurrency(double amount) {
-        return amount * conversionFactor;
-    }
-
-    private double convertToNonBaseCurrency(Currency currency, double amount) {
         return amount * ( this.conversionFactor / currency.conversionFactor);
     }
 }
